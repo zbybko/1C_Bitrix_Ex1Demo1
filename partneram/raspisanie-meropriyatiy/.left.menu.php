2c5f87643b4b87d466b3a77babe95d53d787a2fb
Zakhar <zbybko@wejet.ru>
2024-07-21 20:08:16 +0200

    [ex1-4] Создать шаблон сайта

---
+++ partneram/raspisanie-meropriyatiy/.left.menu.php
@@ -0,0 +1,18 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Анонсы",
+		"/partneram/raspisanie-meropriyatiy/index.php",
+		Array(), 
+		Array(), 
+		"" 
+	),
+  Array(
+    "Архив",
+    "/partneram/raspisanie-meropriyatiy/arkhiv.php",
+    Array(),
+    Array(),
+    ""
+  )
+);
+?>
